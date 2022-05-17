81295665faded5c41d66aa5a90c387f3e8c9755f
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2022-05-17 14:15:18 -0700

    test

--- src/skosConceptScheme.js
+++ src/skosConceptScheme.js
@@ -14,7 +14,7 @@ export default {
   fields: [
     {
       name: 'title',
-      title: 'Taxonomy Concept Scheme',
+      title: 'Taxonomy Concept Scheme — test',
       type: 'string',
       description:
         'Concept schemes are used to group concepts into defined sets, such as thesauri, classification schemes, or facets. Concepts may belong on many (or no) concept schemes, and you may create as many (or few) concept schemes as you like.',
