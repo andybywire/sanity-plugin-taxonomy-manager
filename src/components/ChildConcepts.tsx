fc3fbf1130d797cb5a73140d2421a08e90ccf06c
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2023-05-04 14:46:30 -0700

    Chore: Validate and provide messaging for 5 level hierarchy limit.

--- src/components/ChildConcepts.tsx
+++ src/components/ChildConcepts.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import {Tooltip, Box, Stack, Text} from '@sanity/ui'
+import {ErrorOutlineIcon} from '@sanity/icons'
 import {ChildConceptTerm} from '../types'
 
 const StyledChildConcept = styled.ul`
@@ -6,6 +8,9 @@ const StyledChildConcept = styled.ul`
   li {
     font-weight: normal;
     margin-top: 0.75rem;
+    svg {
+      color: #f03e2f; // red-500
+    }
   }
 `
 
@@ -15,8 +20,28 @@ export const ChildConcepts = ({concepts}: {concepts: ChildConceptTerm[]}) => {
       {concepts.map((concept: any) => {
         return (
           <li key={concept.id}>
-            {concept.prefLabel}
-            {concept.childConcepts?.length > 0 && (
+            {concept.prefLabel} — {concept.level}
+            {concept.childConcepts?.length > 0 && concept.level == 5 && (
+              <Tooltip
+                content={
+                  <Box padding={2} sizing="border">
+                    <Stack padding={1} space={2}>
+                      <Text muted size={1}>
+                        This concept has unlisted child concepts.
+                      </Text>
+                      <Text muted size={1}>
+                        The maximum hierarchy depth is 5 levels.
+                      </Text>
+                    </Stack>
+                  </Box>
+                }
+                fallbackPlacements={['right', 'left']}
+                placement="top"
+              >
+                <ErrorOutlineIcon />
+              </Tooltip>
+            )}
+            {concept.childConcepts?.length > 0 && concept.level < 5 && (
               <ChildConcepts concepts={concept.childConcepts} />
             )}
           </li>
@@ -25,3 +50,5 @@ export const ChildConcepts = ({concepts}: {concepts: ChildConceptTerm[]}) => {
     </StyledChildConcept>
   )
 }
+
+// {concept.level == 5 && <span> *</>}
