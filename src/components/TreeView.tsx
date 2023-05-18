7295e3fb69d839521fd0f0bcbe344219610e2496
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2023-05-18 16:43:30 -0700

    Refactor useCreateConcept as a hook

--- src/components/TreeView.tsx
+++ src/components/TreeView.tsx
@@ -5,90 +5,25 @@
  * taxonomy documents.
  */
 
-import {useClient} from 'sanity'
-import {Container, Stack, Box, Text, Inline, Button, useToast} from '@sanity/ui'
+import {Container, Stack, Box, Text, Inline, Button} from '@sanity/ui'
 import {AddIcon} from '@sanity/icons'
 import Hierarchy from './Hierarchy'
 import {useCallback} from 'react'
-import {randomKey} from '@sanity/util/content'
-import {useOpenNewConceptPane} from '../hooks/useOpenNewConceptPane'
+import {useCreateConcept} from '../hooks/useCreateConcept'
 
 // Need to pass into this component:
 // - documentId with draft status ✔︎
 // - baseUri ✔︎
-// – show new concepts in draft mode (and note that they need to be published)
-// - update tree when new concepts (draft) are published
-// - properly type the document that's passed in
+// – show new concepts in draft mode (and note that they need to be published — not this on a tool tip on "Untitled")
+// - update tree when new concepts (draft) are published ✔︎
+// - properly type the document that's passed in ✔︎
 // - add 'unpublished' badge to newly added concepts not yet published
 // - disable add concept at 5th level; add info icon and tooltip message (or AccessDeniedIcon)
 // - on delete (RemoveCircleIcon), remove from tree and add message in toast: "Concept removed from concept scheme, but not deleted from your concept store. [View Concept] to delete or modify"
 
 export const TreeView = ({document}: {document: any}) => {
   const documentId = document.displayed._id
-  const schemaBaseIri = document.displayed.baseIri
-
-  // console.log(document)
-
-  const client = useClient({apiVersion: '2021-10-21'})
-  const toast = useToast()
-
-  const openInNewPane = useOpenNewConceptPane()
-
-  const createConcept = useCallback(
-    (conceptType: string) => {
-      const skosConcept = {
-        _id: `sc-${randomKey(6)}`,
-        _type: 'skosConcept',
-        prefLabel: '',
-        baseIri: schemaBaseIri,
-        broader: [],
-        related: [],
-      }
-
-      toast.push({
-        closable: true,
-        status: 'info',
-        title: 'Creating top concept',
-        description: 'This message is just a test.',
-      })
-
-      client
-        .transaction()
-        .create(skosConcept)
-        // .patch(draftDoc)
-        // .set({testValue: true})
-        // .patch(draftDoc, (patch) => patch.set({testValue: topConcept._id}))
-        .patch(documentId, (patch) => {
-          if (conceptType == 'topConcept') {
-            return patch
-              .setIfMissing({topConcepts: []})
-              .insert('after', 'topConcepts[-1]', [{_ref: skosConcept._id, _type: 'reference'}])
-          }
-          return patch
-            .setIfMissing({concepts: []})
-            .insert('after', 'concepts[-1]', [{_ref: skosConcept._id, _type: 'reference'}])
-        })
-        .commit({autoGenerateArrayKeys: true})
-        .then((res) => {
-          toast.push({
-            closable: true,
-            status: 'success',
-            title: 'Top concept created',
-            description: `Top concept created.`,
-          })
-          openInNewPane(res.documentIds[0])
-        })
-        .catch((err) => {
-          toast.push({
-            closable: true,
-            status: 'error',
-            title: 'Error creating top concept',
-            description: err.message,
-          })
-        })
-    },
-    [schemaBaseIri, toast, client, documentId, openInNewPane]
-  )
+  const createConcept = useCreateConcept(document)
 
   const createTopConcept = useCallback(() => {
     createConcept('topConcept')
---
+++ src/hooks/useCreateConcept.tsx
@@ -0,0 +1,70 @@
+import {useClient} from 'sanity'
+import {useToast} from '@sanity/ui'
+import {useCallback} from 'react'
+import {randomKey} from '@sanity/util/content'
+import {useOpenNewConceptPane} from './useOpenNewConceptPane'
+
+export function useCreateConcept(document: any) {
+  const toast = useToast()
+  const client = useClient({apiVersion: '2021-10-21'})
+  const openInNewPane = useOpenNewConceptPane()
+  const documentId = document.displayed._id
+  const schemaBaseIri = document.displayed.baseIri
+
+  const createConcept = useCallback(
+    (conceptType: string) => {
+      const skosConcept = {
+        _id: `sc-${randomKey(6)}`,
+        _type: 'skosConcept',
+        prefLabel: '',
+        baseIri: schemaBaseIri,
+        broader: [],
+        related: [],
+      }
+
+      toast.push({
+        closable: true,
+        status: 'info',
+        title: 'Creating top concept',
+        description: 'This message is just a test.',
+      })
+
+      client
+        .transaction()
+        .create(skosConcept)
+        // .patch(draftDoc)
+        // .set({testValue: true})
+        // .patch(draftDoc, (patch) => patch.set({testValue: topConcept._id}))
+        .patch(documentId, (patch) => {
+          if (conceptType == 'topConcept') {
+            return patch
+              .setIfMissing({topConcepts: []})
+              .insert('after', 'topConcepts[-1]', [{_ref: skosConcept._id, _type: 'reference'}])
+          }
+          return patch
+            .setIfMissing({concepts: []})
+            .insert('after', 'concepts[-1]', [{_ref: skosConcept._id, _type: 'reference'}])
+        })
+        .commit({autoGenerateArrayKeys: true})
+        .then((res) => {
+          toast.push({
+            closable: true,
+            status: 'success',
+            title: 'Top concept created',
+            description: `Top concept created.`,
+          })
+          openInNewPane(res.documentIds[0])
+        })
+        .catch((err) => {
+          toast.push({
+            closable: true,
+            status: 'error',
+            title: 'Error creating top concept',
+            description: err.message,
+          })
+        })
+    },
+    [schemaBaseIri, toast, client, documentId, openInNewPane]
+  )
+  return createConcept
+}
