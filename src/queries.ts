e1ee0b2349f94b223defe624b1f0f281e06e4588
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2023-11-03 13:32:58 -0700

    Update branchBuilder to support create-in-place draft documents

--- src/queries.ts
+++ src/queries.ts
@@ -4,35 +4,30 @@
  * @todo: Detect when a 6th level is present and print a message in the UI.
  */
 
-// _id in coalesce(*[_id == 'drafts.' + $id][0], *[_id == $id][0]).concepts[]._ref
-
-// Branch Builder
-// - Get the prefLabel and _id of each concept in the array of concepts referenced by the broader term
-// - Repeat the process for each of those concepts
-// - Run tree to a depth of 6 levels. Return 5.
-// - If there is a 6th level, do no return it; return a message. (TBD)
+/**
+ * Branch Builder
+ * For create-in-place of draft documents, branch builder needs to
+ * query and display draft versions of documents that are registered
+ * in this array as published. Here's how the query works:
+ * - get the any "published" document in the .concepts[] array that has
+ *   a `drafts.` version, or
+ * - get any published document in the .concepts[] array, except those
+ *   with a `conceptId` in the "drafts" list. This eliminates returning
+ *   both the draft and published versions of the same document.
+ * - in both cases, returned documents must have the _id of the parent
+ *   of this child in their .broader[] array.
+ */
 const branchBuilder = (level = 1): string | void => {
   if (level > 6) {
     return ''
   }
-  // Fix: only return the id—-don't coalesce drafts/non-drafts ...
-  // need to get child concepts that are listed as published, but which only have draft versions
-  //current: getting concepts whose _id is in
-  //  the published conceptScheme ([0]) list of concepts, and
-  //  that references the _id of the enclosing document (the concept scheme) in broader[]
-  // problem is that this document doesn't yet exist as a published document
-  // all those documents who have an _id in
-
-  // this works:
-  // "dedupedConcepts": *[
-  //   _id in *[_id == $id][0].concepts[]{"ref": "drafts." + _ref}.ref ||
-  //   (
-  //     _id in *[_id == $id][0].concepts[]._ref &&
-  //     !(conceptId in *[_id in *[_id == $id][0].concepts[]{"ref": "drafts." + _ref}.ref].conceptId)
-  //   )
-  //  ]{prefLabel, _id, conceptId},
-
-  return `"childConcepts": *[_id in *[_id == $id][0].concepts[]._ref && ^._id in broader[]._ref ]|order(prefLabel){
+  return `"childConcepts": *[
+    (_id in *[_id == $id][0].concepts[]{"ref": "drafts." + _ref}.ref ||
+      (
+        _id in *[_id == $id][0].concepts[]._ref &&
+        !(conceptId in *[_id in *[_id == $id][0].concepts[]{"ref": "drafts." + _ref}.ref].conceptId)
+      )
+    ) && ^._id in broader[]._ref ]|order(prefLabel){
       "id": _id,
       "level": ${level},
       prefLabel,
@@ -41,15 +36,6 @@ const branchBuilder = (level = 1): string | void => {
       scopeNote,
       ${branchBuilder(level + 1)}
     }`
-  // return `"childConcepts": *[_id in *[_id == $id][0].concepts[]._ref && ^._id in broader[]._ref ]|order(prefLabel){
-  //   "id": _id,
-  //   "level": ${level},
-  //   prefLabel,
-  //   definition,
-  //   example,
-  //   scopeNote,
-  //   ${branchBuilder(level + 1)}
-  // }`
 }
 // Input Branch Builder displays only published concepts
 const inputBranchBuilder = (level = 1): string | void => {
@@ -67,16 +53,17 @@ const inputBranchBuilder = (level = 1): string | void => {
     }`
 }
 
-// Trunk Builder
-// Fetch the top concepts and their child concepts and orphans and their child concepts.
-// coalesce() returns the first non-null value in the list of arguments, so either the draft or the published concept.
-// To get orphans:
-// - filter to concepts in this scheme only
-// - filter out concepts that reference a topConcept in this scheme as a broader term
-// - filter out concepts that reference other concepts in this scheme as a broader term
 /**
- * @param {id} string the skosConceptScheme document _id
- * @returns query string
+ * Trunk Builder
+ * Fetch the top concepts and their child concepts and orphans and
+ * their child concepts. coalesce() returns the first non-null value
+ * in the list of arguments, so either the draft or the published concept.
+ * To get orphans:
+ * - filter to concepts in this scheme only
+ * - filter out concepts that reference a topConcept in this scheme as
+ *   a broader term
+ * - filter out concepts that reference other concepts in this scheme
+ *   as a broader term
  */
 export const trunkBuilder = (): string => {
   return `*[_id == $id][0] {
@@ -108,8 +95,9 @@ export const trunkBuilder = (): string => {
 
 /**
  * Input Builder
- * Accept a branchId parameter, and filter to topConcepts and Orphans in that branch only.
- * Then call branchBuilder recursively — it will only build terms in the scheme referenced by that concept
+ * Accept a branchId parameter, and filter to topConcepts and Orphans
+ * in that branch only. Then call branchBuilder recursively — it will
+ * only build terms in the scheme referenced by that concept.
  * - branchBuilder() and inputBuilder() are called in Hierarchy.tsx
  * @todo vet functionality of orphans
  */
