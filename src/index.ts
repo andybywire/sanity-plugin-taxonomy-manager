c38917ef0785a00c7f8ad65f8874481120751cef
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2024-08-24 10:06:00 -0700

    Update plugin kit packages and tsconfig

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,8 @@ interface Options {
 
 /**
  * Defines a Sanity plugin for managing taxonomies.
- * baseURI should follow an IANA http/s scheme and should terminate with either a / or #.
+ *
+ * BaseURI should follow an IANA http/s scheme and should terminate with either a / or #.
  * @param options - Optional configuration options for the plugin.
  * @param options.baseUri - The base URI to use for SKOS concepts and concept schemes.
  * @returns A Sanity plugin object.
