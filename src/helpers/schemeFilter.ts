0c82d0a73fabf1b0f31c715d2baef84beb17ea84
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-04-04 16:02:57 -0700

    Annotate & improve documentation

--- src/helpers/schemeFilter.ts
+++ src/helpers/schemeFilter.ts
@@ -11,39 +11,59 @@ type SchemeFilterResult = {
 }
 
 /**
- * #### Document Scheme Filter
+ * #### Reference Field Scheme Filter
  * Pluggable Function for Filtering to a Single SKOS Concept Scheme
- * @param schemeId - The unique six character concept identifier for the Concept Scheme to which you wish to filter.
- * @returns A reference type filter for Concepts and Top Concepts in the selected Concept Scheme
+ * @param schemeId - The unique six character concept identifier for
+ *   the Concept Scheme to which you wish to filter.
+ * @returns A reference type filter for Concepts and Top Concepts in
+ *   the selected Concept Scheme test
+ * @example
+ * ```ts
+ * import { schemeFilter } from 'sanity-plugin-taxonomy-manager'
+ * ...
+ * {
+ *   name: 'test',
+ *   type: 'array',
+ *   of: [
+ *     {
+ *       type: 'reference',
+ *       to: {type: 'skosConcept'},
+ *       options: {
+ *         filter: schemeFilter({schemeId: 'b6f990'}),
+ *         disableNew: true,
+ *       },
+ *     },
+ *   ],
+ * }
+ * ```
  */
-export function schemeFilter(
-  tmOptions: SchemeOptions
-): ({
-  getClient,
-}: {
-  getClient: (options: {apiVersion: string}) => any
-}) => Promise<SchemeFilterResult> {
-  const {schemeId} = tmOptions || {}
+export const schemeFilter = (
+  options: SchemeOptions
+): (({getClient}: {getClient: Function}) => Promise<SchemeFilterResult>) => {
+  // Get and validate the schemeId from options
+  const {schemeId} = options || {}
   if (!schemeId || typeof schemeId !== 'string') {
     throw new Error('Invalid or missing schemeId: scheme Id must be a string')
   }
-  return async ({
-    getClient,
-  }: {
-    getClient: (options: {apiVersion: string}) => any
-  }): Promise<SchemeFilterResult> => {
+
+  return async ({getClient}) => {
     const client = getClient({apiVersion: '2023-01-01'})
+    if (!client) {
+      throw new Error('Client not available')
+    }
+    // Fetch concepts and topConcepts for the given schemeId
     const {concepts, topConcepts} = await client.fetch(
       `{
-        "concepts": *[_type=="skosConceptScheme" && schemeId == "${schemeId}"].concepts[]._ref,
-        "topConcepts": *[_type=="skosConceptScheme" && schemeId == "${schemeId}"].topConcepts[]._ref
-      }`
+      "concepts": *[_type=="skosConceptScheme" && schemeId == "${schemeId}"].concepts[]._ref,
+      "topConcepts": *[_type=="skosConceptScheme" && schemeId == "${schemeId}"].topConcepts[]._ref
+    }`
     )
+    // schemeId is included in params for use by the ArrayHierarchyInput component
     return {
       filter: `!(_id in path("drafts.**"))
-      && _id in $concepts
-      || _id in topConcepts`,
-      params: {concepts, topConcepts},
+              && _id in $concepts
+              || _id in topConcepts`,
+      params: {concepts, topConcepts, schemeId},
     }
   }
 }
