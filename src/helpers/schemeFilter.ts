319aa8e69ca0917bc5fd24fa0ea78a30eb11af2c
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-04-04 12:15:46 -0700

    Update schemeFilter.ts helper to use async filter
    - Required to support field level copy/paste in Studio

--- src/helpers/schemeFilter.ts
+++ src/helpers/schemeFilter.ts
@@ -4,7 +4,10 @@ type SchemeOptions = {
 
 type SchemeFilterResult = {
   filter: string
-  params: SchemeOptions
+  params: {
+    concepts: string[]
+    topConcepts: string[]
+  }
 }
 
 /**
@@ -13,14 +16,34 @@ type SchemeFilterResult = {
  * @param schemeId - The unique six character concept identifier for the Concept Scheme to which you wish to filter.
  * @returns A reference type filter for Concepts and Top Concepts in the selected Concept Scheme
  */
-export function schemeFilter(options: SchemeOptions): SchemeFilterResult {
-  const {schemeId} = options || {}
-  return {
-    filter: `!(_id in path("drafts.**"))
-    && _id in *[_type=="skosConceptScheme" && schemeId == $schemeId].concepts[]._ref
-    || _id in *[_type=="skosConceptScheme" && schemeId == $schemeId].topConcepts[]._ref`,
-    params: {
-      schemeId: schemeId,
-    },
+export function schemeFilter(
+  tmOptions: SchemeOptions
+): ({
+  getClient,
+}: {
+  getClient: (options: {apiVersion: string}) => any
+}) => Promise<SchemeFilterResult> {
+  const {schemeId} = tmOptions || {}
+  if (!schemeId || typeof schemeId !== 'string') {
+    throw new Error('Invalid or missing schemeId: scheme Id must be a string')
+  }
+  return async ({
+    getClient,
+  }: {
+    getClient: (options: {apiVersion: string}) => any
+  }): Promise<SchemeFilterResult> => {
+    const client = getClient({apiVersion: '2023-01-01'})
+    const {concepts, topConcepts} = await client.fetch(
+      `{
+        "concepts": *[_type=="skosConceptScheme" && schemeId == "${schemeId}"].concepts[]._ref,
+        "topConcepts": *[_type=="skosConceptScheme" && schemeId == "${schemeId}"].topConcepts[]._ref
+      }`
+    )
+    return {
+      filter: `!(_id in path("drafts.**"))
+      && _id in $concepts
+      || _id in topConcepts`,
+      params: {concepts, topConcepts},
+    }
   }
 }
