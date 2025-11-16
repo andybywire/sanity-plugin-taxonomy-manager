3ee4badbfdef6546135ec37b6fbcd435516dacad
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-11-16 08:05:16 -0800

    Remove unused color helpers

--- src/hooks/useLinkColorScheme.tsx
+++
@@ -1,8 +0,0 @@
-import {hues} from '@sanity/color'
-import {useColorSchemeValue} from 'sanity'
-
-export function useLinkColorScheme() {
-  const colorScheme = useColorSchemeValue()
-  if (colorScheme == 'dark') return hues.gray[50].hex
-  return hues.gray[800].hex
-}
